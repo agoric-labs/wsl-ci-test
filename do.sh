de530e4c8b4dc17b5e1cabcc414d071cea93cf51
Brian Warner <warner@lothar.com>
2020-04-20 21:26:16 -0700

    more

--- do.sh
+++ do.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -e
 
-echo "-- doing node --version"
-node.exe --version
-echo "-- doing yarn --version"
-yarn --version
-echo "-- doing npm --version"
-npm.exe --version
+#echo "-- doing node --version"
+#node.exe --version
+# plain "node --version" fails because the WSL/appveyor one has a .exe extension
+
+#echo "-- doing yarn --version"
+#yarn --version
+# the WSL/appveyor yarn (/mnt/c/Program Files something/Yarn/bin/yarn) doesn't recognize
+# the WSL/appveyor npm, it complains "Yarn requires Node.js 4.0 or higher to be installed"
+
+#echo "-- doing npm --version"
+#npm --version
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+echo "-- doing wget install"
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+ls -l ~/.nvm
 
