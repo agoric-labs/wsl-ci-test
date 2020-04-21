d4b387105a5c0b19288e95f43897b9e7e5c837f7
Brian Warner <warner@lothar.com>
2020-04-20 21:50:53 -0700

    more

--- install-nvm.sh
+++ install-nvm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+type git
 echo "-- doing wget install"
 (wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash) || exit 0
 ls -l ~/.nvm
