2a21eff087fe3917c78b96da6f085adbcd1e45c1
Brian Warner <warner@lothar.com>
2020-04-22 10:53:06 -0700

    more

--- install-nvm.sh
+++ install-nvm.sh
@@ -2,21 +2,18 @@
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 type git
-echo "-- doing wget install"
-#wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh
-echo "this goes to stderr" >&2
-echo " running install.sh"
-#bash install.sh
-echo "install.sh done" $?
-
-echo "contents of ~/.nvm:"
-#ls -l ~/.nvm
-
-#echo "warner- waiting for ssh"
-#curl -sflL 'https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-ssh.sh' | bash -e -
 
 echo "downloading NVM install.sh"
 wget -q https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh 2>&1
 
 echo "running install.sh"
 bash install.sh 2>&1
+echo "install.sh done" $?
+
+#echo "this goes to stderr" >&2
+
+echo "contents of ~/.nvm:"
+ls -l ~/.nvm
+
+#echo "warner- waiting for ssh"
+#curl -sflL 'https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-ssh.sh' | bash -e -
