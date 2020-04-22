4f4a5018add85ceb52ac5cac7780e0f36f6cbcee
Brian Warner <warner@lothar.com>
2020-04-21 17:15:52 -0700

    more

--- install-nvm.sh
+++ install-nvm.sh
@@ -5,7 +5,7 @@ type git
 echo "-- doing wget install"
 wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh
 echo " running install.sh"
-bash install.sh
+#bash install.sh
 echo "install.sh done" $?
 
 echo "contents of ~/.nvm:"
