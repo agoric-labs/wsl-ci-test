fcc2738341b4a52cd9b90f80edde28c0f38b35b8
Brian Warner <warner@lothar.com>
2020-04-23 14:37:29 -0700

    more

--- run-test.sh
+++ run-test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -ie
 
 cd agoric-sdk
-git checkout origin/950-fix-lmdb-crash-on-wls
+#git checkout origin/950-fix-lmdb-crash-on-wls
 
 cd packages/swing-store-lmdb
+yarn test
+
+cd ../..
+
+echo "-- top-level yarn build"
+yarn build
+
+echo "-- top-level yarn test"
 yarn test
